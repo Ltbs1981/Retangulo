2e43ddb347a55cf1cbf9ab86ae3da2d20ea8e8de
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-09 19:32:28 -0300

    Adicionada a class CalcR

---
+++ CalcR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PerimetroRetangulo
+{
+    class CalcR
+    {
+        public double altura;
+        public double largura;
+
+        public double CalculandoPerimetro()
+        {
+            return (largura + altura) * 2;
+        }
+
+        public double CalculandoArea()
+        {
+            return largura * altura;
+        }
+
+        public double CalculandoDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,30 +13,20 @@ namespace PerimetroRetangulo
             Console.WriteLine("Digite a largura do retângulo");
             double largura = double.Parse(Console.ReadLine());
 
-            double perimetro = CalculandoPerimetro(altura, largura);
-            double area = CalculandoArea(altura, largura);
-            double diagonal = CalculandoDiagonal(altura, largura);
-            diagonal = Math.Round(diagonal, 2);
+            CalcR Calculador = new CalcR();
+            Calculador.altura = altura;
+            Calculador.largura = largura;
 
+            double perimetro = Calculador.CalculandoPerimetro();
+            double area = Calculador.CalculandoArea();
+            double diagonal = Calculador.CalculandoDiagonal();
+            perimetro = Math.Round(perimetro, 2);
+            area = Math.Round(area, 2);
+            diagonal = Math.Round(diagonal, 2);
 
             Console.WriteLine($"Perímetro: {perimetro}");
             Console.WriteLine($"Área: {area}");
             Console.WriteLine($"Diagonal: {diagonal}");
-        }
-
-        public static double CalculandoPerimetro(double altura, double largura)
-        {
-            return 2 * (largura + altura);
-        }
-
-        public static double CalculandoArea(double altura, double largura)
-        {
-            return largura * altura;
-        }
-
-        public static double CalculandoDiagonal(double altura, double largura)
-        {
-            return Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
         }
     }
 }
