ddd5bf39e4402303c44bcd3c893efa1793e1f07d
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-09 15:47:08 -0300

    3ª fase, inserindo métodos

--- Program.cs
+++ Program.cs
@@ -8,20 +8,35 @@ namespace PerimetroRetangulo
         {
             Console.WriteLine("Calculando a área de um retângulo");
             Console.WriteLine("\nDigite a altura do retângulo");
-            double altura = double.Parse(Console.ReadLine()) ;
+            double altura = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Digite a largura do retângulo");
-            double largura = double.Parse(Console.ReadLine()) ;
+            double largura = double.Parse(Console.ReadLine());
 
-            double perimetro = 2 * (largura + altura);
-            double area = largura * altura;
-            double diagonal = Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
-
+            double perimetro = CalculandoPerimetro(altura, largura);
+            double area = CalculandoArea(altura, largura);
+            double diagonal = CalculandoDiagonal(altura, largura);
             diagonal = Math.Round(diagonal, 2);
+
 
             Console.WriteLine($"Perímetro: {perimetro}");
             Console.WriteLine($"Área: {area}");
             Console.WriteLine($"Diagonal: {diagonal}");
+        }
+
+        public static double CalculandoPerimetro(double altura, double largura)
+        {
+            return 2 * (largura + altura);
+        }
+
+        public static double CalculandoArea(double altura, double largura)
+        {
+            return largura * altura;
+        }
+
+        public static double CalculandoDiagonal(double altura, double largura)
+        {
+            return Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
         }
     }
 }
